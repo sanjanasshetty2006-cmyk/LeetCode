0d07666e798dbc6e7721e4c7e097dc4cf429eec3
sanjanasshetty2006-cmyk <sanjanasshetty2006@gmail.com>
2026-07-29 19:44:52 +0530

    Time: 4 ms (45.73%), Space: 12.4 MB (21.78%) - LeetHub

---
+++ 0008-string-to-integer-atoi/0008-string-to-integer-atoi.py
@@ -0,0 +1,39 @@
+class Solution(object):
+    def myAtoi(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+
+        s = s.lstrip()
+
+        if not s:
+            return 0
+
+        sign = 1
+        i = 0
+
+        if s[i] == '-':
+            sign = -1
+            i += 1
+        elif s[i] == '+':
+            i += 1
+
+        num = 0
+
+        while i < len(s) and s[i].isdigit():
+            num = num * 10 + int(s[i])
+            i += 1
+
+        num *= sign
+
+        INT_MIN = -2**31
+        INT_MAX = 2**31 - 1
+
+        if num < INT_MIN:
+            return INT_MIN
+
+        if num > INT_MAX:
+            return INT_MAX
+
+        return num
