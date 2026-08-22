da12a5a47c9625267150265776a2ba795a2eada4
sanjanasshetty2006-cmyk <sanjanasshetty2006@gmail.com>
2026-08-22 18:18:36 +0530

    Time: 0 ms (100%), Space: 12.5 MB (20.06%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,14 @@
+class Solution(object):
+    def climbStairs(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+
+        one = 1
+        two = 1
+
+        for i in range(n):
+            one, two = two, one + two
+
+        return one
